573c48d563acbc839aedbcd973d6079c81b4b241
kajdabrowski <kaj.dabrowskiki@gmail.com>
2021-11-04 11:54:50 +0100

    FOR THE LOVE OF GOD

--- app/src/App.js
+++ app/src/App.js
@@ -47,7 +47,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
