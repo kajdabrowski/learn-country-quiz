aa103359ac4255b94c2fc6c5ebe49392513240af
splarsxd <splarsiz34@yahoo.com>
2021-11-05 15:24:42 +0100

    Fixed featureFlags

--- app/src/featureFlags.js
+++ app/src/featureFlags.js
@@ -1,22 +1,11 @@
-
-
 const initFeatures = () => {
 
-    if(!localStorage.getItem("featureFlags")) {
-        localStorage.setItem("featureFlags", JSON.stringify([
-           
+    if(!localStorage.getItem("featureFlags")) {localStorage.setItem("featureFlags", JSON.stringify([
             {name: "ffImprovedScoring", description: "Improved Scoring", active: false},
             {name: "ffImprovedRandomizer", description: "Improved Randomizer", active: false},
             {name: "ffImprovedTies", description: "Improved Ties", active: false}
-
-
         ]))
     }
-
-
 }
-
-
-
 
 export default initFeatures;
